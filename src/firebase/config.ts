80c2f9f8218ca3bec92a509204bbb9b748b4f0d2
Severino <severino.perfumeshop@gmail.com>
2026-05-06 18:59:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3715293407-4e40c",
+  "appId": "1:589447134453:web:e73c4a0dc133ae68f359bc",
+  "apiKey": "AIzaSyARSONh9sC0tf85yuL0lX23a1_hNWcTJlw",
+  "authDomain": "studio-3715293407-4e40c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "589447134453"
+};
